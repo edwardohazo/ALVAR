3d76366d434cbd30d979e371363d9aba55bca7eb
Eduardo <newguyonthemmarket@gmail.com>
2023-01-08 18:58:04 -0600

    first commit frontend dir and gitignore

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ALVAR | MUSIC</a>
+      </header>
+      <main>list songs</main>
+    </div>
+  );
+}
+
+export default App;
